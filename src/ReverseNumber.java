f48d8c59a03be277dee1384fb28d3a040c8de3a7
xiaopiqiu <xzhaoshuai@gmail.com>
2022-02-16 22:33:34 +0800

    电话号码的字母组合

--- src/ReverseNumber.java
+++ src/ReverseNumber.java
@@ -1,3 +1,4 @@
+import java.net.PortUnreachableException;
 import java.util.*;
 
 public class ReverseNumber {
@@ -195,6 +196,76 @@ public class ReverseNumber {
             }
         }
         return ans;
+    }
+
+    public int threeSumClosest(int[] nums, int target) {
+        Arrays.sort(nums);
+        int count = nums.length;
+        int res = 10000000;
+        for (int i = 0; i < count; i++) {
+            if (i > 0 && nums[i] == nums[i - 1]) {
+                continue;
+            }
+            int left = i + 1, right = count - 1;
+            while (left < right) {
+                int sum = nums[i] + nums[left] + nums[right];
+                if (sum == target) {
+                    return sum;
+                }
+
+                if (Math.abs(sum - target) < Math.abs(res - target)) {
+                    res = sum;
+                };
+                if (sum > target) {
+                    int k = right - 1;
+                    while (left < k && nums[right] == nums[k]) {
+                        --k;
+                    }
+                    right = k;
+                } else {
+                    int k = left + 1;
+                    while (k < right && nums[left] == nums[k]) {
+                        ++k;
+                    }
+                    left = k;
+                }
+            }
+        }
+        return res;
+    }
+
+    public List<String> letterCombinations(String digits) {
+        List<String> combinations = new ArrayList<String>();
+        if (digits.length() == 0) {
+            return combinations;
+        }
+        Map<Character, String> phoneMap = new HashMap<Character, String>() {{
+            put('2', "abc");
+            put('3', "def");
+            put('4', "ghi");
+            put('5', "jkl");
+            put('6', "mno");
+            put('7', "pqrs");
+            put('8', "tuv");
+            put('9', "wxyz");
+        }};
+
+        backtrack(combinations, phoneMap, digits, 0, new StringBuffer());
+        return combinations;
+    }
+    public void backtrack(List<String> combinations, Map<Character, String> phoneMap, String digits, int index, StringBuffer combination) {
+        if (index == digits.length()) {
+            combinations.add(combination.toString());
+        } else  {
+            char digit = digits.charAt(index);
+            String letters = phoneMap.get(digit);
+            int lettersCount = letters.length();
+            for (int i = 0; i < lettersCount; i++) {
+                combination.append(letters.charAt(i));
+                backtrack(combinations, phoneMap, digits, index + 1, combination);
+                combination.deleteCharAt(index);
+            }
+        }
     }
     public static void main(String args[]) {
         ReverseNumber r = new ReverseNumber();
