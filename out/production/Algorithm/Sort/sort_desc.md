478b810f25acf10fa6cd01667db9ed0483104913
xiaopiqiu <xzhaoshuai@gmail.com>
2022-11-29 21:06:04 +0800

    快速排序和归并排序

--- out/production/Algorithm/Sort/sort_desc.md
+++ out/production/Algorithm/Sort/sort_desc.md
@@ -14,36 +14,3 @@
 稳定性。这个概念是说，如果待排序的序列中存在值相等的元素，经过排序之后，相等元素之间原有的先后顺序不变。
 
 [排序算法介绍](https://mp.weixin.qq.com/s/HQg3BzzQfJXcWyltsgOfCQ)
-
-## 快速排序
-快速排序的逻辑是，若要对 nums[lo..hi] 进行排序，我们先找一个分界点 p，通过交换元素使得 nums[lo..p-1] 都小于等于 nums[p]，且 nums[p+1..hi] 都大于 nums[p]，然后递归地去 nums[lo..p-1] 和 nums[p+1..hi] 中寻找新的分界点，最后整个数组就被排序了。
-```
-void sort(int[] nums, int lo, int hi) {
-    /****** 前序遍历位置 ******/
-    // 通过交换元素构建分界点 p
-    int p = partition(nums, lo, hi);
-    /************************/
-
-    sort(nums, lo, p - 1);
-    sort(nums, p + 1, hi);
-}
-```
-
-再说说归并排序的逻辑，若要对 nums[lo..hi] 进行排序，我们先对 nums[lo..mid] 排序，再对 nums[mid+1..hi] 排序，最后把这两个有序的子数组合并，整个数组就排好序了。
-
-归并排序的代码框架如下:
-```
-// 定义：排序 nums[lo..hi]
-void sort(int[] nums, int lo, int hi) {
-    int mid = (lo + hi) / 2;
-    // 排序 nums[lo..mid]
-    sort(nums, lo, mid);
-    // 排序 nums[mid+1..hi]
-    sort(nums, mid + 1, hi);
-
-    /****** 后序位置 ******/
-    // 合并 nums[lo..mid] 和 nums[mid+1..hi]
-    merge(nums, lo, mid, hi);
-    /*********************/
-}
-```
--- src/Sort/Sort.java
+++ src/Sort/Sort.java
@@ -1,12 +1,15 @@
 package Sort;
 
+import java.util.Arrays;
+import java.util.Random;
+
 public class Sort {
 
     /**
     * 冒泡排序
     * 原地排序，稳定排序算法，最好是O(n) 最坏是O(n^2)
     * */
-    public void bubbleSort(int[] nums) {
+    public int[] bubbleSort(int[] nums) {
         for (int i = 0; i < nums.length; i++) {
             /// 提前退出冒泡循环标志
             boolean flag = false;
@@ -20,13 +23,14 @@ public class Sort {
             }
             if (!flag) break; // 没有数据交换直接退出
         }
+        return nums;
     }
 
     /**
      * 插入排序
      * 原地排序，稳定排序算法，最好是O(n) 最坏是O(n^2)
      * */
-    public void insertSort(int[] nums) {
+    public int[] insertSort(int[] nums) {
         for (int i = 0; i < nums.length; i++) {
             int value = nums[i];
             int j = i - 1;
@@ -40,9 +44,10 @@ public class Sort {
             }
             nums[j+1] = value;// 插入数据
         }
+        return nums;
     }
 
-    public void selectionSort(int[] nums) {
+    public int[] selectionSort(int[] nums) {
         for (int i = 0; i < nums.length; i++) {
             int minIndex = i;
             for (int j = i; j < nums.length; j++) {
@@ -54,7 +59,101 @@ public class Sort {
             nums[minIndex] = nums[i];
             nums[i] = temp;
         }
+        return nums;
     }
 
+    public int[] shellSort(int[] nums) {
+        int len = nums.length;
+        int gap = len / 2;
+        while (gap > 0) {
+            for (int i = gap; i < len; i++) {
+                int temp = nums[i];
+                int preIndex = i - gap;
+                while (preIndex >= 0 && nums[preIndex] > temp) {
+                    nums[preIndex + gap] = nums[preIndex];
+                    preIndex -= gap;
+                }
+                nums[preIndex + gap] = temp;
+            }
+            gap /= 2;
+        }
+        return nums;
+    }
 
+    public int[] mergeSort(int[] nums) {
+        if (nums.length < 2) return nums;
+        int mid = nums.length / 2;
+        int[] left = Arrays.copyOfRange(nums, 0, mid);
+        int[] right = Arrays.copyOfRange(nums, mid, nums.length);
+        return _merge(mergeSort(left), mergeSort(right));
+    }
+    public int[] _merge(int[] left, int[] right) {
+        int[] res = new int[left.length + right.length];
+        for (int index = 0, i = 0, j = 0; index < res.length; index++) {
+            if (i >= left.length) {
+                res[index] = right[j++];
+            } else if (j >= right.length) {
+                res[index] = left[i++];
+            } else if (left[i] > right[j]) {
+                res[index] = right[j++];
+            } else {
+                res[index] = left[i++];
+            }
+        }
+        return res;
+    }
+
+    public int[] quickSort(int[] nums) {
+        if (nums.length <= 1) return nums;
+        return quickSortHelper(nums, 0, nums.length - 1);
+    }
+
+    public int[] quickSortHelper(int[] nums, int left, int right) {
+        if (left > right) {
+            return nums;
+        }
+        int smallIndex = partition(nums, left, right);
+        if (smallIndex > left) {
+            quickSortHelper(nums, left, smallIndex - 1);
+        }
+
+        if (smallIndex < right) {
+            quickSortHelper(nums, smallIndex + 1, smallIndex);
+        }
+        return nums;
+    }
+    public int partition(int[] array, int start, int end) {
+        int pivot = (int) (start + Math.random() * (end - start + 1));
+        int smallIndex = start - 1;
+        swap(array, pivot, end);
+        for (int i = start; i <= end; i++)
+            if (array[i] <= array[end]) {
+                smallIndex++;
+                if (i > smallIndex)
+                    swap(array, i, smallIndex);
+            }
+        return smallIndex;
+    }
+
+    public static void swap(int[] array, int i, int j) {
+        int temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
+    void printArray(int[] nums) {
+        for (int i = 0; i < nums.length; i++) {
+            System.out.print(nums[i]);
+            System.out.print(", ");
+        }
+        System.out.println();
+    }
+    static public void main(String[] args) {
+        Sort s = new Sort();
+        int[] nums = new int[]{3, 6, 12, 5, 4, 1};
+        s.printArray(s.bubbleSort(nums));
+        s.printArray(s.insertSort(nums));
+        s.printArray(s.selectionSort(nums));
+        s.printArray(s.mergeSort(nums));
+        s.printArray(s.quickSort(nums));
+    }
 }
