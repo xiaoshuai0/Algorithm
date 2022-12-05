535328072574b14cd8c41ad91ec0a57905374f77
xiaopiqiu <xzhaoshuai@gmail.com>
2022-12-05 21:44:10 +0800

    子集

--- src/BackTrack/BackTrackSolution.java
+++ src/BackTrack/BackTrackSolution.java
@@ -31,4 +31,30 @@ public class BackTrackSolution {
             used[i] = false;
         }
     }
+    List<List<Integer>> subsets_res = new LinkedList<>();
+    LinkedList<Integer> track = new LinkedList<>();
+
+    public List<List<Integer>> subsets(int[] nums) {
+        subsets_backTrack(nums, 0);
+        return subsets_res;
+    }
+
+    public void subsets_backTrack(int[] nums, int start) {
+        // 前序位置，每个节点的值都是一个子集
+        subsets_res.add(new LinkedList<>(track));
+
+        for (int i = start; i < nums.length; i++) {
+            // 做选择
+            track.addLast(nums[i]);
+            // 通过 start 控制树枝遍历，避免重复
+            subsets_backTrack(nums, i + 1);
+            // 撤销选择
+            track.removeLast();
+        }
+    }
+
+    public static void main(String[] args) {
+        BackTrackSolution s = new BackTrackSolution();
+        System.out.println(s.subsets(new int[] {1, 2, 3}));
+    }
 }
